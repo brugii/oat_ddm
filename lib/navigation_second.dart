1806db25eb9fa5f5e67002cfd19f6ef5d51535f9
jvmds <104597673+jvmds@users.noreply.github.com>
2023-06-20 09:53:56 -0300

    Update navigation_second.dart

--- lib/navigation_second.dart
+++ lib/navigation_second.dart
@@ -12,34 +12,7 @@ class NavigationSecond extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buscar IP'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Container(
-          child: Column(
-            children: [
-              proterty('IP:', ip.ip),
-              const Spacer(),
-              proterty('Hostname:', ip.hostname),
-              const Spacer(),
-              proterty('City:', ip.city),
-              const Spacer(),
-              proterty('Region:', ip.region),
-              const Spacer(),
-              proterty('Country:', ip.country),
-              const Spacer(),
-              proterty('Loc:', ip.loc),
-              const Spacer(),
-              proterty('Org:', ip.org),
-              const Spacer(),
-              proterty('Postal:', ip.postal),
-              const Spacer(),
-              proterty('Timezone:', ip.timezone),
-              const Spacer(),
-              proterty('Readme:', ip.readme),
-            ],
-          ),
-        ),
-      ),
+      body: //
     );
   }
 
